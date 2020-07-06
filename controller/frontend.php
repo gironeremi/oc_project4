40ee4cbecebac749f7de6ba7bdd75b25d9e73818
Remi GIRONE <gironeremi@gmail.com>
2020-07-06 09:31:32 +0200

    Ajout de la fonction cleanVar et mise en place d'un switch/case au lieu des if en cascade.

--- controller/frontend.php
+++ controller/frontend.php
@@ -1,32 +1,53 @@
 <?php
 require_once('model/PostManager.php');
 require_once('model/CommentManager.php');
-function listPosts()
-{
+function listPosts() {
     $postManager = new OpenClassrooms\Blog\Model\PostManager();
     $posts = $postManager->getPosts();
     require('view/frontend/listPostsView.php');
 }
-function post()
-{
-    $postManager = new OpenClassrooms\Blog\Model\PostManager();
-    $commentManager = new OpenClassrooms\Blog\Model\CommentManager();
+function post() {
+    if (isset($_GET['id']) && $_GET['id'] >0) {
+        $postManager = new OpenClassrooms\Blog\Model\PostManager();
+        $commentManager = new OpenClassrooms\Blog\Model\CommentManager();
 
-    $post = $postManager->getPost($_GET['id']);
-    $comments = $commentManager->getComments($_GET['id']);
-    require('view/frontend/postView.php');
+        $post = $postManager->getPost($_GET['id']);
+        $comments = $commentManager->getComments($_GET['id']);
+        require('view/frontend/postView.php');
+    } else {
+        throw new Exception('acucun identifiant de billet envoyé.');
+    }
 }
-function addComment ($postId, $author, $comment)
+function addComment ($postId, $author, $comment) {
+    $postId = cleanVar($_GET['id']);
+    $author = cleanVar($_POST['author']);
+    $comment = cleanVar($_POST['comment']);
+    if ((isset($postId)) && $postId > 0) {
+        if (!empty($author) && !empty($comment)) {
+            $commentManager = new OpenClassrooms\Blog\Model\CommentManager();
+            $affectedLines = $commentManager->postComment($postId, $author, $comment);
+            if ($affectedLines === false)
+            {
+                throw new Exception('Impossible d\'ajouter le commentaire.');
+            }
+            else
+            {
+                header('Location: index.php?action=post&id=' . $postId);
+            }
+        }
+        else {
+            throw new Exception('tous les champs ne sont pas remplis !');
+        }
+    }
+}
+function cleanVar($str)
 {
-    $commentManager = new OpenClassrooms\Blog\Model\CommentManager();
-
-    $affectedLines = $commentManager->postComment($postId, $author, $comment);
-    if ($affectedLines === false)
+    if(isset($str))
     {
-        throw new Exception('Impossible d\'ajouter le commentaire.');
+        return trim(htmlspecialchars($str));
     }
     else
     {
-        header('Location: index.php?action=post&id=' . $postId);
+        return "";
     }
 }
