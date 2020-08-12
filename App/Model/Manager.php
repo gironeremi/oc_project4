bc7511094b189ecff669475212a1f29f41718ad3
Remi GIRONE <gironeremi@gmail.com>
2020-08-12 14:28:18 +0200

    Déplacement des données de connexions à la base de données en clair.

--- App/Model/Manager.php
+++ App/Model/Manager.php
@@ -1,11 +1,12 @@
 <?php
 namespace App\Model;
+require('config.php');
 class Manager
 {
 //ici mettre un require pour chercher mes identifiants.
     protected function dbConnect()
     {
-        $db= new \PDO('mysql:host=localhost;port=3308;dbname=p4', 'root', '', array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION));
+        $db= new \PDO(DB_HOST, DB_USER, DB_PASS, array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION));
         return $db;
     }
 }
---
+++ App/Model/config.php
@@ -0,0 +1,4 @@
+<?php
+const DB_HOST = 'mysql:host=localhost;port=3308;dbname=p4';
+const DB_USER = 'root';
+const DB_PASS ='';
